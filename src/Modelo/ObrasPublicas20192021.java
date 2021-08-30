8fb02160727d8c3bb64ff1e2f69391969c9e5958
AngelGonx <angelsebastian1103@gmail.com>
2021-08-30 17:17:13 -0500

    Creación del proyecto, plantilla.

---
+++ src/Modelo/ObrasPublicas20192021.java
@@ -0,0 +1,12 @@
+package Modelo;
+/**
+ * @author Ángel González Rincón
+ * Date: 30-ago-2021
+ */
+public class ObrasPublicas20192021 {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
