4ea6a3f0b36ac90c4ba2b3f3e38553f3c6ad7861
AngelGonx <angelsebastian1103@gmail.com>
2021-09-02 00:33:53 -0500

    Creación de todas las plantillas de diseño para el proyecto, además de algunas funcionalidades de botones.

--- src/Modelo/ObrasPublicas20192021.java
+++ src/Modelo/ObrasPublicas20192021.java
@@ -1,4 +1,7 @@
 package Modelo;
+
+import Vista.Principal.accesoPrincipal;
+
 /**
  * @author Ángel González Rincón
  * Date: 30-ago-2021
@@ -6,6 +9,25 @@ package Modelo;
 public class ObrasPublicas20192021 {
 
     public static void main(String[] args) {
+        
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Windows".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(ObrasPublicas20192021.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(ObrasPublicas20192021.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(ObrasPublicas20192021.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(ObrasPublicas20192021.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        accesoPrincipal mainPrincipal = new accesoPrincipal();
+        mainPrincipal.show();        
         
     }
     
