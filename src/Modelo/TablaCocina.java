932c3546d80d47bfe717b5f467ca67a52ae4f2ad
CarlosChanMendez <77695013+CarlosChanMendez@users.noreply.github.com>
2021-10-16 08:49:17 -0500

    Se agrego la carpeta Modelo y los modelos

---
+++ src/Modelo/TablaCocina.java
@@ -0,0 +1,32 @@
+package Modelo
+//created on 16-10-2021 at 8:12:54
+//class 'TablaCocina'
+public class TablaCocina{
+// variables for TablaCocina
+
+	private int idtabla_cocina;
+	private String codigo_cocina;
+
+
+// setters and getters for TablaCocina
+
+// setter method for 'idtabla_cocina'
+public void setidtabla_cocina(int idtabla_cocina){
+    this.idtabla_cocina = idtabla_cocina;
+}
+
+// getter method for 'idtabla_cocina'
+public int getidtabla_cocina(int idtabla_cocina){
+    return this.idtabla_cocina = idtabla_cocina;
+}
+// setter method for 'codigo_cocina'
+public void setcodigo_cocina(String codigo_cocina){
+    this.codigo_cocina = codigo_cocina;
+}
+
+// getter method for 'codigo_cocina'
+public String getcodigo_cocina(String codigo_cocina){
+    return this.codigo_cocina = codigo_cocina;
+}
+
+}
